040db0bf93ec1f2cf16405a44b2251b38b07c186
Adam Hauze <hauze_v@protonmail.com>
2020-11-15 11:21:14 -0500

    Finished Game.js module init function which renders the initial game screen.

--- JS/modules/game.js
+++ JS/modules/game.js
@@ -16,7 +16,44 @@ const Game = (() => {
 
     /* Build out our own guessing word to render */
     guessingWord = Array(chosenWord.length).fill("_"); // creates an Array of length chosenWord.length and fills it with underscores
+
+    /* Basic game state setup */
+    guessedLetters = [];
+    lives = 7;
+
+    /* Render initial Game screen */
+    showInitPage();
   }
+
+  const showInitPage = () => {
+    let markup = `
+      <p class="hangman__stats">Lives:
+        <span class="hangman__lives">${lives}</span>
+      </p>
+      <h1 class="hangman__title">Hangman</h1>
+      <canvas class="hangman__board" height="155px"></canvas>
+      <div class="hangman__word">${guessingWord.join("")}</div>
+      <p class="hangman__instructions">Pick a letter below to guess the whole word.</p>
+      <ul class="hangman__letters">
+        ${createLetters()}
+      </ul>
+      <button class="button hangman__trigger">Main Menu</button>
+    `
+
+    $hangman.innerHTML = markup;
+  }
+
+  const createLetters = () => {
+    /* Loop through the letters array and for each letter, create an li tag with class hangman__letter, then return the markup */
+    let markup = ``;
+    letters.forEach(letter => {
+      markup += `
+        <li class="hangman__letter">${letter}</li>
+      `
+    });
+
+    return markup;
+  }    
 
   const chooseWord = () => {
     return words[Math.floor((Math.random() * words.length))];
